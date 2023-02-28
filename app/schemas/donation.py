264d4672e4349f9fcbd04ebfd46b043c6b6c84ea
unknown <vladsizov96@yandex.ru>
2023-02-28 23:54:50 +0300

    hw done

---
+++ app/schemas/donation.py
@@ -0,0 +1,25 @@
+from datetime import datetime
+from pydantic import BaseModel, Field, PositiveInt
+from typing import Optional
+
+
+class DonationBase(BaseModel):
+    full_amount: PositiveInt
+    comment: Optional[str]
+
+
+class DonationCreate(DonationBase):
+    id: int
+    create_date: datetime
+
+    class Config:
+        orm_mode = True
+
+
+class DonationDB(DonationCreate):
+    id: int
+    create_date: datetime
+    user_id: int
+    invested_amount: int = Field(0)
+    fully_invested: bool
+    close_date: Optional[datetime]
